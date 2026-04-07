d66271bbd2e64c4d1845a45c0de2dede03cafab9
Archana0427 <archana0chowdary@gmail.com>
2026-04-07 16:12:10 +0530

    uc6 coded to console

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,26 +1,19 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
-
-        // If no arguments are provided
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-            return;
-        }
-
-        // Using StringBuilder to build the greeting
         StringBuilder nameBuilder = new StringBuilder();
-        boolean first = true;
 
         for (String name : args) {
-            if (!first) {
-                nameBuilder.append(", ");
-            }
-            nameBuilder.append(name);
-            first = false;
+            nameBuilder.append(name).append(", ");
         }
 
-        // Display final output
-        System.out.println("Hello, " + nameBuilder.toString() + "!");
+        if (nameBuilder.length() > 0) {
+            // Remove last ", "
+            String result = nameBuilder.substring(0, nameBuilder.length() - 2);
+            System.out.println("Hello " + result);
+        } else {
+            System.out.println("Hello World");
+        }
     }
 }
